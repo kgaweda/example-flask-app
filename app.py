ff256dafcf5044979a6bf48d62f45c82f54776e3
kgaweda <kamil.gaweda@outlook.com>
2024-11-11 16:37:32 +0100

    convert to json response

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/", methods=['GET'])
+def helloApi():
+    data = {
+        "name": "Hello world"
+    }
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
